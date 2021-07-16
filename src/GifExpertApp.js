6671258fd80519aa331f71aa918fdb3d23065fc2
Eder <chlrobert93@gmail.com>
2021-07-16 18:27:41 -0500

    GitExpertApp-Component

---
+++ src/GifExpertApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GifExpertApp = () => {
+
+    return (
+       <div>
+           <h2>Gif Expert</h2>
+           <hr />
+       </div>
+    );
+};
+
+export default GifExpertApp;
